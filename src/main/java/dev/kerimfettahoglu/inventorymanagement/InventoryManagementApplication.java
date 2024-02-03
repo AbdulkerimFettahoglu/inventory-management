b665145b289f37ff91dc861547331c81eae55b86
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2024-02-03 23:42:58 +0300

    csrf disabled to test our webservices from postman instead of browser.

--- src/main/java/dev/kerimfettahoglu/inventorymanagement/InventoryManagementApplication.java
+++ src/main/java/dev/kerimfettahoglu/inventorymanagement/InventoryManagementApplication.java
@@ -2,12 +2,24 @@ package dev.kerimfettahoglu.inventorymanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class InventoryManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryManagementApplication.class, args);
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest()
+						.permitAll())
+				.csrf(AbstractHttpConfigurer::disable);
+		return http.build();
 	}
 
 }
