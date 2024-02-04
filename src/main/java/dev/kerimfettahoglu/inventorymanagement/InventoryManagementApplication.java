aac28700eb077b206ab6433f781bb5d45de31c66
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2024-02-04 22:07:39 +0300

    initial data added.

--- src/main/java/dev/kerimfettahoglu/inventorymanagement/InventoryManagementApplication.java
+++ src/main/java/dev/kerimfettahoglu/inventorymanagement/InventoryManagementApplication.java
@@ -1,5 +1,12 @@
 package dev.kerimfettahoglu.inventorymanagement;
 
+import dev.kerimfettahoglu.inventorymanagement.api.input.ProductNewInput;
+import dev.kerimfettahoglu.inventorymanagement.entity.Category;
+import dev.kerimfettahoglu.inventorymanagement.entity.Product;
+import dev.kerimfettahoglu.inventorymanagement.service.CategoryService;
+import dev.kerimfettahoglu.inventorymanagement.service.ProductService;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +14,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @SpringBootApplication
 public class InventoryManagementApplication {
 
@@ -14,6 +22,29 @@ public class InventoryManagementApplication {
 		SpringApplication.run(InventoryManagementApplication.class, args);
 	}
 
+	private final CategoryService categoryService;
+	private final ProductService productService;
+
+	@PostConstruct
+	private void initData() {
+		Category category1 = new Category();
+		category1.setName("cat1");
+		Category category2 = new Category();
+		category2.setName("cat2");
+		category1 = categoryService.create(category1);
+		category2 = categoryService.create(category2);
+		ProductNewInput pro1 = new ProductNewInput();
+		pro1.setCategoryId(category1.getId());
+		pro1.setName("pro1");
+		pro1.setBrand("pro1");
+		Product p1 = productService.create(pro1);
+		ProductNewInput pro2 = new ProductNewInput();
+		pro2.setCategoryId(category2.getId());
+		pro2.setName("pro2");
+		pro2.setBrand("pro2");
+		Product p2 = productService.create(pro2);
+
+	}
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest()
