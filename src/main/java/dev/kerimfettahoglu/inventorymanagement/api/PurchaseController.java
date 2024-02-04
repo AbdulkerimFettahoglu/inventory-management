a4fff5e20febee7db63f7119c615ccf06256d1fa
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2024-02-04 13:05:30 +0300

    crud operations for purchase entity.

---
+++ src/main/java/dev/kerimfettahoglu/inventorymanagement/api/PurchaseController.java
@@ -0,0 +1,40 @@
+package dev.kerimfettahoglu.inventorymanagement.api;
+
+import dev.kerimfettahoglu.inventorymanagement.api.input.PurchaseNewInput;
+import dev.kerimfettahoglu.inventorymanagement.api.input.PurchaseUpdateInput;
+import dev.kerimfettahoglu.inventorymanagement.entity.Purchase;
+import dev.kerimfettahoglu.inventorymanagement.service.PurchaseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("purcahse")
+public class PurchaseController {
+
+    private final PurchaseService purchaseService;
+
+    @PostMapping
+    public ResponseEntity<Purchase> create(@RequestBody PurchaseNewInput input) {
+        return ResponseEntity.ok(purchaseService.create(input));
+    }
+
+    @PutMapping
+    public ResponseEntity<Purchase> update(@RequestBody PurchaseUpdateInput input) {
+        return ResponseEntity.ok(purchaseService.update(input));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Purchase>> get(@RequestParam(required = false) Long id) {
+        return ResponseEntity.ok(purchaseService.getAll(id));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Boolean> delete(@RequestParam Long id) {
+        return ResponseEntity.ok(purchaseService.delete(id));
+    }
+
+}
